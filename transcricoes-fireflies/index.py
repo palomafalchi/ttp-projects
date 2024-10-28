6dd70a9c7884c25ba47b2f1fa58432d2cc226a9f
palomafalchi <paloma.falchi@zup.com.br>
2024-10-28 17:27:32 -0300

    adicionado codigo atualizado em index.py

--- transcricoes-fireflies/index.py
+++ transcricoes-fireflies/index.py
@@ -1,9 +1,10 @@
 import boto3
 import requests
 from fpdf import FPDF
+import json
+import os
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-import os
 import json
 
 # Configurações
@@ -15,37 +16,55 @@ fireflies_api_key = os.getenv('API_KEY_FIREFLIES')
 # Cliente S3
 s3 = boto3.client('s3', region_name='sa-east-1')
 
+def lambda_handler(event, context):
+    # Ler os dados salvos no arquivo JSON
+    try:
+        with open('transcripts.json', 'r') as f:
+            transcripts = json.load(f)
+    except FileNotFoundError:
+        print("Arquivo 'transcripts.json' não encontrado. Execute a função 'fetch_transcripts()' primeiro.")
+        return
+
+    # Processar cada transcrição e gerar o PDF
+    for item in transcripts:
+        print('item', item)
+
+        formatted_date = format_date(item['dateString'])
+        pdf_content = f"{item['title']} - {formatted_date}\n\n"
+        
+        # Atualizar o pdf_content com as informações da transcrição
+        pdf_content = get_sentences(pdf_content, item['id'])
+        pdf_filename = f"{formatted_date}-{item['id']}.pdf"
+        
+        # Gerar o PDF com o conteúdo atualizado
+        pdf_path = format_pdf(pdf_content, pdf_filename)
+
+        print(f"PDF gerado em handleer depois format_pdf: {pdf_path}")
+        
+        save_pdf_s3(pdf_path, pdf_filename)
+
 def format_pdf(content, filename):
     try:
         pdf = FPDF()
         pdf.add_page()
-        pdf.set_font("Arial", size=12)
+        pdf.set_auto_page_break(auto=True, margin=15)
+        pdf.set_font("Helvetica", size=12)
+
         # Defina a largura da célula para a largura da página menos as margens
-        page_width = pdf.w - 2 * pdf.l_margin
-        previous_speaker = None
-        
-        for line in content.split('\n'):
-            if previous_speaker and line.startswith(previous_speaker):
-                # Se a linha começa com o nome do speaker anterior, remova o nome do speaker
-                line = line[len(previous_speaker):].strip()
-            elif ':' in line:
-                # Se a linha contém ':', coloque o nome do speaker em negrito
-                speaker, text = line.split(':', 1)
-                pdf.set_font("Arial", style='B', size=12)
-                pdf.multi_cell(page_width, 10, txt=speaker + ":", align='L')
-                pdf.set_font("Arial", size=12)
-                line = text.strip()
-                previous_speaker = speaker
-            pdf.multi_cell(page_width, 10, txt=line, align='L')
-        
+        page_width = pdf.epw  # epw é a largura da área de conteúdo da página
+
+        # Adiciona o conteúdo diretamente ao PDF
+        pdf.multi_cell(page_width, 10, content)
+
+        # Salva o PDF no caminho especificado
         pdf_output = f"/tmp/{filename}"
         pdf.output(pdf_output)
-        print('pdf_output',pdf_output)    
+        print('PDF salvo em FORMAT PDF:', pdf_output)
         return pdf_output
     except Exception as e:
         print(f"Erro ao criar PDF: {e}")
         raise
-
+  
 def format_date(date_string):
     try:
         # Converter a string para um objeto datetime
@@ -56,7 +75,7 @@ def format_date(date_string):
         print(f"Erro ao formatar data: {e}")
         raise
 
-def get_transcription_infos(pdf_content, id): 
+def get_sentences(pdf_content, id): 
     print(id)
     payload = f"{{\"query\":\"query Transcript($transcriptId: String!) {{ transcript(id: $transcriptId) {{ title id dateString sentences {{ index speaker_name text }} }} }}\",\"variables\":{{\"transcriptId\":\"{id}\"}}}}"
         
@@ -70,85 +89,29 @@ def get_transcription_infos(pdf_content, id):
     if response.status_code == 200:
         data = json.loads(response.text)
         transcript = data['data']["transcript"]
-        print('transcript',transcript)
-        
+        print('transcript', transcript)
+
         previous_speaker = ""
         for sentence in transcript['sentences']:
-            if previous_speaker: 
+            if previous_speaker:
                 if sentence['speaker_name'] == previous_speaker:
-                    pdf_content = pdf_content.rstrip('\n') + f" {sentence['text']}\n"
+                    pdf_content = pdf_content.rstrip('\n\n') + f" {sentence['text']}\n\n"
                 else:
-                    pdf_content += f"{sentence['speaker_name']}: {sentence['text']}\n"
+                    pdf_content += f"{sentence['speaker_name']}: {sentence['text']}\n\n"
             previous_speaker = sentence['speaker_name']
-            
-        return pdf_content
-
-def lambda_handler(event, context):
-
-    try:
-        # Obter o horário atual
-        current_time = datetime.now()
-        print('current_time.hour',current_time.hour)
-        if current_time.hour == 9:
-            # Se for 9h, 15 horas antes pra buscar a partir das 18h do dia anterior
-            current_time_adjusted = current_time - timedelta(hours=15)
-        else:
-            # Caso contrário, 3 horas antes
-            current_time_adjusted = current_time - timedelta(hours=3)
         
-        # Adicionar 3 horas ao horário atual
-        current_time_plus_3 = current_time_adjusted + timedelta(hours=3)
-        # Converter para o formato ISO 8601
-        current_time_iso = current_time_plus_3.isoformat()
-        current_time_iso = "2024-10-24T16:19:39.117680"
-        print(current_time_iso)
+        return pdf_content
+    else:
+        print(f"Erro na requisição: {response.status_code}")
+        return pdf_content  # Retornar o conteúdo original se a requisição falhar
 
-        payload = f"{{\"query\":\"query Transcripts($fromDate: DateTime) {{ transcripts(fromDate: $fromDate) {{ title id dateString }} }}\",\"variables\":{{\"fromDate\":\"{current_time_iso}\"}}}}"
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f"Bearer {fireflies_api_key}"
-        }
-
-        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
-
-        # Verificar se a requisição foi bem-sucedida
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            print('data',data)
-            transcripts = data['data']["transcripts"]
-            
-            if len(transcripts) > 0:
-                for item in data['data']["transcripts"]:
-                    print('item',item)
-                    formatted_date = format_date(item['dateString'])
-
-                    # Gerar o conteúdo do PDF
-                    pdf_content = f"{item['title']} - {formatted_date}\n\n"
-                    pdf_content = get_transcription_infos(pdf_content, item['id']);
-    
-                    # Criar o PDF
-                    pdf_filename = f"{item['id']}.pdf"
-                    pdf_path = format_pdf(pdf_content, pdf_filename)
-                    print(f"PDF path: {pdf_path}")
-                    print('pdf_content',pdf_content)        
-                    # Salvar o PDF no S3
-                    
-                    try:
-                        s3.head_bucket(Bucket=bucket_name)
-                        print(f"Conexão com o bucket {bucket_name} estabelecida com sucesso.")
-                    except Exception as e:
-                        print(f"Erro ao conectar ao bucket S3: {e}")
-                    
-                    try:
-                        with open(pdf_path, "rb") as pdf_file:
-                            s3.put_object(Bucket=bucket_name, Key=pdf_filename, Body=pdf_file)
-                        print(f"PDF salvo no S3: {pdf_filename}")
-                    except Exception as e:
-                        print(f"Erro ao salvar o PDF no S3: {e}")
-
-        else:
-            print('Falha na requisição para o Fireflies.')
-    except requests.exceptions.RequestException as e:
-        print(f"Erro na requisição: {e}")
-    except Exception as e:
-        print(f"Erro geral: {e}")
+def save_pdf_s3(pdf_path, pdf_filename):
+            # Salvar o PDF no S3
+        s3 = boto3.client('s3', region_name='sa-east-1')
+        bucket_name = os.getenv('BUCKET_S3')
+        try:
+            with open(pdf_path, "rb") as pdf_file:
+                s3.put_object(Bucket=bucket_name, Key=pdf_filename, Body=pdf_file)
+            print(f"PDF salvo no S3: {pdf_filename}")
+        except Exception as e:
+            print(f"Erro ao salvar o PDF no S3: {e}")
